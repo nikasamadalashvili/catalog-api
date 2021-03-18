e586c34d0f1ea30bffc623402fcce8dd312cfcea
Nika Samadalashvili <nika.samadalashvili@wandio.com>
2021-03-18 23:13:24 +0400

    init

---
+++ src/Catalog.Infrastructure/CatalogContext.cs
@@ -0,0 +1,33 @@
+﻿using Catalog.Domain.Entities;
+using Catalog.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Catalog.Infrastructure
+{
+    public class CatalogContext : DbContext, IUnitOfWork
+    {
+        public const string DEFAULT_SCHEMA = "catalog";
+
+        public DbSet<Item> Items { get; set; }
+
+        public CatalogContext(DbContextOptions<CatalogContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+
+        public async Task<bool> SaveEntitiesAsync(CancellationToken cancellationToken = default)
+        {
+            await SaveChangesAsync(cancellationToken);
+
+            return true;
+        }
+    }
+}
---
+++ src/Catalog.Infrastructure/Repositories/ItemRepository.cs
@@ -0,0 +1,54 @@
+﻿using Catalog.Domain.Entities;
+using Catalog.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Infrastructure.Repositories
+{
+    public class ItemRepository : IItemRepository
+    {
+        private readonly CatalogContext _context;
+
+        public IUnitOfWork UnitOfWork => _context;
+
+        public ItemRepository(CatalogContext catalogContext)
+        {
+            _context = catalogContext ?? throw new ArgumentNullException(nameof(CatalogContext));
+        }
+
+        public Item Add(Item order)
+        {
+            return _context.Items.Add(order).Entity;
+        }
+
+        public async Task<IEnumerable<Item>> GetAsync()
+        {
+            return await _context
+                .Items
+                .AsNoTracking()
+                .ToListAsync();
+        }
+
+        public async Task<Item> GetAsync(Guid id)
+        {
+            var item = await _context.Items
+                .AsNoTracking()
+                .Where(x => x.Id == id)
+                .Include(x => x.Genre)
+                .Include(x => x.Artist).FirstOrDefaultAsync();
+
+            return item;
+        }
+
+        public Item Update(Item item)
+        {
+            _context.Entry(item).State = EntityState.Modified;
+
+            return item;
+        }
+    }
+}
